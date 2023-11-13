776b273d1b79a6a7addcbe6ca4307f11e4aac3d7
Alexandre Halas <alexandrehalas@gmail.com>
2023-11-12 22:21:55 -0300

    Helpers and Configuration Files

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,14 +4,6 @@ import recipeView from './views/recipeView.js';
 import 'core-js/stable'; //polyfilling other things
 import 'regenerator-runtime/runtime'; //polyfilling async/await
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
